456f3db920cabfd09a22af12772e19a51bdd041e
Benoit Asselin <benoit@161.io>
2016-09-21 16:09:05 +0200

    License

--- js/plugins/bs-eb-type-element-other1.js
+++ js/plugins/bs-eb-type-element-other1.js
@@ -2,7 +2,7 @@
 /*!
 Bootstrap Email Builder v0.0.1
 Copyright (c) 161 SARL, http://161.io
-License : github.com/161io/bootstrap-email-builder/license.txt
+License: https://github.com/161io/bootstrap-email-builder/blob/master/license.txt
  */
 
 (function() {
